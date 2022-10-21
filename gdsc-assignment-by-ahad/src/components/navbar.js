f41047f954223c0995d4d550fe78f696ac87a78d
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2022-10-22 00:51:35 +0500

    Mobile Responsiveness To Navbar

--- gdsc-assignment-by-ahad/src/components/navbar.js
+++ gdsc-assignment-by-ahad/src/components/navbar.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
+
+
 export default function Navbar() {
+
+  const [menuState, setMenuState] = React.useState('menu-close');
+
+  function toggleMenu() {
+    menuState==='menu-open' ? setMenuState('menu-close') : setMenuState('menu-open')
+  }
+
   return (
     <div className="navbar font flex">
       <a href='/'>
@@ -9,9 +18,11 @@ export default function Navbar() {
             <div className="logo-txt">GDSC</div>
         </div>
       </a>
-    <span className="material-symbols-outlined darkM">dark_mode</span>
-    <span className="material-symbols-outlined lightM">light_mode</span>
-      <div className="links flex">
+      <span className="material-symbols-outlined darkM">dark_mode</span>
+      <span className="material-symbols-outlined lightM">light_mode</span>
+      <span className={`material-symbols-outlined hamburger`} onClick={toggleMenu}>drag_handle</span>
+      <span className={`material-symbols-outlined hamburger ${menuState}-ham`} onClick={toggleMenu}>close</span>
+      <div className={`links flex ${menuState}`}>
         <a href="/" className="link">Home</a>
         <a href="/" className="link">About</a>
         <a href="/" className="link">Contact</a>
