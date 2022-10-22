f519f77c6cab41c0e638d8d953e020a07b2e51c5
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2022-10-22 13:08:43 +0500

    Dark Mode Setting

--- gdsc-assignment-by-ahad/src/components/navbar.js
+++ gdsc-assignment-by-ahad/src/components/navbar.js
@@ -5,24 +5,35 @@ import React from 'react'
 export default function Navbar() {
 
   const [menuState, setMenuState] = React.useState('menu-close');
+  const [darkMode, setDarkMode] = React.useState(false);
 
   function toggleMenu() {
     menuState==='menu-open' ? setMenuState('menu-close') : setMenuState('menu-open')
   }
 
+  function themeColorSwitch() {
+    return darkMode ? 'dark-enabled' : 'light-enabled'
+  }
+
+  function themeChange() {
+    darkMode ? setDarkMode(false) : setDarkMode(true)
+  }
+
+  document.body.style.backgroundColor = darkMode ? 'var(--themeFgColor)' : 'var(--themeBgColor)';
+
   return (
-    <div className="navbar font flex">
+    <div className={`navbar font flex ${themeColorSwitch()}`}>
       <a href='/'>
-        <div className="logo flex">
+        <div className={`logo flex ${themeColorSwitch()}`}>
             <span className="material-symbols-outlined logo-img">code</span>
             <div className="logo-txt">GDSC</div>
         </div>
       </a>
-      <span className="material-symbols-outlined darkM">dark_mode</span>
-      <span className="material-symbols-outlined lightM">light_mode</span>
-      <span className={`material-symbols-outlined hamburger`} onClick={toggleMenu}>drag_handle</span>
-      <span className={`material-symbols-outlined hamburger ${menuState}-ham`} onClick={toggleMenu}>close</span>
-      <div className={`links flex ${menuState}`}>
+      <span onClick={themeChange} className={`material-symbols-outlined darkM flex ${themeColorSwitch()}`}>dark_mode</span>
+      <span onClick={themeChange} className={`material-symbols-outlined lightM flex ${themeColorSwitch()}`}>light_mode</span>
+      <span className={`material-symbols-outlined hamburger ${themeColorSwitch()}`} onClick={toggleMenu}>drag_handle</span>
+      <span className={`material-symbols-outlined hamburger ${menuState}-ham ${themeColorSwitch()}`} onClick={toggleMenu}>close</span>
+      <div className={`links flex ${menuState} ${themeColorSwitch()}`}>
         <a href="/" className="link">Home</a>
         <a href="/" className="link">About</a>
         <a href="/" className="link">Contact</a>
